8bd69ee110216bc037b40bd25f96808de0a7fbc2
Dennis Onder <webdev.dennisonder@gmail.com>
2018-11-19 19:42:31 +0100

    Implemented the state in a functional component, added methods for adding new todos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
+const Todo = ({ todo, index }) => {
+  return (
+    <div className="todo">
+      { todo.text }
+    </div>
+  )
+}
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
   }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" placeholder="Add a new todo:" value={value} onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn React Hooks',
+      isCompleted: false
+    },
+    {
+      text: 'Meet friends for lunch',
+      isCompleted: false
+    },
+    {
+      text: 'Build damn cool apps',
+      isCompleted: false
+    }
+  ])
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text}];
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="App">
+      <div className="todo-list">
+        {
+          todos.map((todo, index) => (
+            <Todo key={index} index={index} todo={todo} />
+          ))
+        }
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  )
 }
 
 export default App;
